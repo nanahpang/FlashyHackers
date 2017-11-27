c5a01cff08d3f9277ad39548ec745e86be0e3de0
nanahpang <nanahpang@gmail.com>
2017-11-26 19:28:54 -0500

    update datetime widget

--- MeetingSecretary/schedule/forms.py
+++ MeetingSecretary/schedule/forms.py
@@ -5,12 +5,17 @@ from django.utils.translation import ugettext_lazy as _
 
 from schedule.models import Event, Occurrence
 from schedule.widgets import SpectrumColorPicker
-
+from bootstrap3_datetime.widgets import DateTimePicker
 
 class SpanForm(forms.ModelForm):
-    start = forms.SplitDateTimeField(label=_("start"))
-    end = forms.SplitDateTimeField(label=_("end"),
-                                   help_text=_("The end time must be later than start time."))
+    # start = forms.SplitDateTimeField(label=_("start"))
+    # end = forms.SplitDateTimeField(label=_("end"),
+                                   # help_text=_("The end time must be later than start time."))
+    start = forms.DateTimeField(label=_("start"),required=True,
+                                 widget=DateTimePicker(options={"format": "MM/DD/YYYY HH:mm", "pickSeconds": False}))
+    end = forms.DateTimeField(label=_("end"),required=True,
+                               widget=DateTimePicker(options={"format": "MM/DD/YYYY HH:mm", "pickSeconds": False}),
+                               help_text=_("The end time must be later than start time."))
 
     def clean(self):
         if 'end' in self.cleaned_data and 'start' in self.cleaned_data:
@@ -30,12 +35,15 @@ class EventForm(SpanForm):
     class Meta(object):
         model = Event
         exclude = ('creator', 'created_on', 'calendar')
+        widgets = {'start': forms.DateTimeInput(attrs={'class': 'datepicker'}),
+                    'end': forms.DateTimeInput(attrs={'class': 'datepicker'})}
 
 
 class OccurrenceForm(SpanForm):
     class Meta(object):
         model = Occurrence
         exclude = ('original_start', 'original_end', 'event', 'cancelled')
+
 
 
 class EventAdminForm(forms.ModelForm):
